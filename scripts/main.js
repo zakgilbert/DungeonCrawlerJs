c578906f3d84c3dec23df727b57c78cfb546b072
zakgilbert <gilbertz@hawaii.edu>
2020-03-29 14:35:49 -1000

    add

--- scripts/main.js
+++ scripts/main.js
@@ -23,21 +23,6 @@ canvas.addEventListener("mousemove", inputListeners.mousemove, false);
 window.addEventListener("keydown", inputListeners.keydown, false);
 window.addEventListener("keyup", inputListeners.keyup, false);
 
-/*
-let heroStanding = new sprite(0, 0, 992, 1280, 16, 16, "images/pally_stand.png", "hero");
-let heroReadyStance = new sprite(0, 0, 640, 1712, 16, 8, "images/pally_ready.png", "hero");
-let heroReadyWalk = new sprite(0, 0, 990, 1616, 16, 10, "images/pally_ready_walk.png", "hero");
-let heroWalking = new sprite(0, 0, 1040, 1296, 16, 10, "images/pally_walk.png", "hero");
-let heroRunning = new sprite(0, 0, 752, 1648, 16, 8, "images/pally_run.png", "hero");
-let heroStates = {
-    readyStance: heroReadyStance,
-    readyWalk: heroReadyWalk,
-    walking: heroWalking,
-    running: heroRunning,
-    standing: heroStanding
-};
-*/
-
 
 
 setInterval(() => {
