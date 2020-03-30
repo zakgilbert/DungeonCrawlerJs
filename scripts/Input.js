ff501d671e235c9a62170a4508d0078bad905f0c
zakgilbert <gilbertz@hawaii.edu>
2020-03-29 15:10:02 -1000

    Implement attack_1

--- scripts/Input.js
+++ scripts/Input.js
@@ -10,15 +10,21 @@ function initiateInputListeners(hero) {
         keydown: (e) => {
             switch (e.keyCode) {
                 case 87:
-                    hero.state = (hero.ready ? "readyWalk" : "walking");
+                    hero.state = (hero.ready && !hero.inAction ? "readyWalk" : "walking");
                     break;
                 case 69:
                     hero.ready = !hero.ready;
                     break;
                 case 83:
-                    hero.state = "running";
+                    if (!hero.inAction)
+                        hero.state = "running";
                     break;
                 case 81:
+                    if (hero.ready && hero.state !== "attack_1" && !hero.inAction) {
+                        hero.inAction = true;
+                        hero.previousState = hero.state;
+                        hero.state = "attack_1";
+                    }
                     // q,
                     break;
                 case 67:
@@ -27,10 +33,10 @@ function initiateInputListeners(hero) {
                 default:
                     break;
             }
-            console.log(e.keyCode);
         },
         keyup: (e) => {
-            hero.state = (hero.ready ? "readyStance" : "standing");
+            hero.state = (hero.inAction ? 
+                "attack_1" : (hero.ready ? "readyStance" : "standing"));
         }
     }
 }
